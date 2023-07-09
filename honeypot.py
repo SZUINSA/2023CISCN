bf8b5061f00e448ebde2586fe3164c521ac19182
正汰 <huangzhengdoc@163.com>
2023-07-09 20:54:54 +0800

    update fofa api

--- honeypot.py
+++ honeypot.py
@@ -41,10 +41,50 @@ class method_fofa:
                 else:
                     result = item[2] + "/" + item[3]
                 self.logger.debug(result)
-                self.db.add_honeypot(target,result)
+                self.db.add_honeypot(target, result)
             self.logger.debug("HINEYPOT: fofa run")
         else:
             self.logger.debug("HINEYPOT: fofa error")
+
+
+class method_quake:
+    def __init__(self, db, logger, name="honeypot-fofa"):
+        self.db = db
+        self.logger = logger
+        self.name = name
+        import os
+        import json
+        import requests
+        self.key = os.environ['quake_key']
+        self.proxies = {"http": "", "https": ""}
+        url = "https://quake.360.cn/api/v3/user/info"
+        headers = {"X-QuakeToken": self.key}
+        response = requests.get(url, header=headers, proxies=self.proxies)
+        logger.debug(response.text)
+        json = json.loads(response.text)
+        if json['code'] == 0:
+            self.logger.info("HONEYPOT: quake apikey load")
+        else:
+            self.logger.info("HONEYPOT: quake apikey error")
+
+    def honeypot(self, target):
+        import json
+        import requests
+        url = "https://quake.360.cn/api/v3/search/quake_service"
+        headers = {"X-QuakeToken": self.key}
+        data = {"query": f"app: \"*蜜罐*\" AND ip: \"{target}\""}
+        self.logger.debug("HONEYPOT: quake run")
+        response = requests.post(url, headers=headers, json=data, proxies=self.proxies)
+        json = json.loads(response.text)
+        self.logger.debug(json)
+        if len(json["data"]) != 0:
+            for item in json["data"]:
+                if item["components"][0]["product_name_en"] != "unknown-honeypot":
+                    self.db.add_honeypot(target, f'{item["port"]}/{item["components"][0]["product_name_en"]}')
+                else:
+                    self.db.add_honeypot(target, f'{item["port"]}/N')
+        time.sleep(1)
+
 
 class method_quake_dump:
     def __init__(self, db, logger, name="honeypot-quake-dump"):
@@ -58,19 +98,21 @@ class method_quake_dump:
         except Exception:
             self.logger.info("HONEYPOT: quake_dump_file error")
 
-    def get_ip(self,data):
+    def get_ip(self, data):
         import re
         datastr = str(data)
         pattern_ip = re.compile("\'(\d+\.\d+\.\d+\.\d+)\'")
         ipname_list = re.findall(pattern_ip, datastr)
         return ipname_list
-    def get_honneypot(self,data, ip):
+
+    def get_honneypot(self, data, ip):
         import re
         datastr = str(data[ip])
         pattern_honneypot = re.compile("\s\'(\d+\/.*?)\'")
         honneypot_list = re.findall(pattern_honneypot, datastr)
         return honneypot_list
-    def honeypot(self,target):
+
+    def honeypot(self, target):
         import os
         import json
         filepath = self.path
@@ -79,8 +121,8 @@ class method_quake_dump:
             try:
                 with open(filepath + filename) as file:
                     data = json.load(file)
-                    #ip_list = get_ip(data)
-                    honeypot_list = self.get_honneypot(data,target)
+                    # ip_list = get_ip(data)
+                    honeypot_list = self.get_honneypot(data, target)
                     for honeypot in honeypot_list:
                         try:
                             self.db.add_honeypot(target, honeypot)
@@ -88,7 +130,6 @@ class method_quake_dump:
                             self.logger.warning("HONEYPOT: ip in file not available")
             except Exception:
                 pass
-
 
 class app:
     def __init__(self, db, logger, method='honeypot-fofa'):
@@ -106,17 +147,17 @@ class app:
         while True:
             ip = self.db.get_ip_no_services(self.method.name)
             if ip is not None:
-                self.logger.info("HONEYPOT-CHECK %s %s" % (self.method.name,ip,))
-                self.db.update_ip_scan_timestamp(self.method.name,ip)
-            
+                self.logger.info("HONEYPOT-CHECK %s %s" % (self.method.name, ip,))
+                self.db.update_ip_scan_timestamp(self.method.name, ip)
+
                 result = self.method.honeypot(ip)
                 try:
-                    self.db.add_ip(ip,result)
+                    self.db.add_ip(ip, result)
                     self.logger.debug(str(result))
                 except Exception:
                     pass
-            
-                self.logger.info("HONEYPOT-CHECK %s %s SUCCESS" % (self.method.name,ip,))
+
+                self.logger.info("HONEYPOT-CHECK %s %s SUCCESS" % (self.method.name, ip,))
             else:
                 self.logger.debug("HONEYPOT: sleep")
                 time.sleep(sleep)
